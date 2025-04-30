1137dc5a3ed59e92a8ecdb7109017fcb18ecb07f
adavorn <jacob46king@gmail.com>
2025-04-30 09:08:32 -0500

    added js code for api

--- YT-api/js.js
+++ YT-api/js.js
@@ -1,1 +1,19 @@
 const apiKey = process.env.REACT_APP_API_KEY
+
+const search = document.getElementById("searchButton")
+
+const googleAPIUrl = "https://www.googleapis.com/youtube/v3/search"
+
+searchButton.addEventListener("click", (e) => {
+
+    const searchTerm = document.getElementById("searchQuery").value;
+
+    const apiPrefix = "&key=";
+
+    const searchQuery = "?part=snippet&q=" + searchTerm + apiPrefix + apiKey;
+
+    const url = googleAPIUrl + searchQuery;
+    
+    fetch(url);
+
+});
