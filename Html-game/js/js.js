46bc10fdf20e32d8a6b8762310c8a4c675adfb64
adavorn <jacob46king@gmail.com>
2025-05-01 11:51:53 -0500

    added sprite movement

--- Html-game/js/js.js
+++ Html-game/js/js.js
@@ -9,8 +9,33 @@ player.src = "player.png";
 
 var x = 0;
 var y = 0;
+var key = 0;
 
 player.onload = function() {
     ctx.drawImage(player, x, y, 75, 75);
-
 }
+
+document.onkeydown = function(e) {
+    key = e.key;
+}
+
+
+setInterval(function() {
+
+    if (key == "w") {
+        y -= 1;
+    }
+    if (key == "a") {
+        x -= 1;
+    }
+    if (key == "s") {
+        y += 1;
+    }
+    if (key == "d") {
+        x += 1;
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(player, x, y, 75, 75);
+
+}, 5);
